683df414e6aedf3bb0f11a33d6b3e5c5b5683059
Christian Lück <christian@lueck.tv>
2013-09-08 17:56:58 +0200

    Use mootools class interface

--- web/messenger.js
+++ web/messenger.js
@@ -1,34 +1,36 @@
-var Messenger = function(target) {
-	this.target = target;
-	this.connection = null;
-	
-	this.connect = function (target) {
+var Messenger = new Class({
+    initialize: function(target){
+    	this.target = target;
+    	this.connect(target);
+    },
+    
+	connect: function (target) {
 		this.connection = new WebSocket('ws://' + target);
 		this.connection.onmessage = function(e) {
 			this.onData(JSON.parse(e.data));
 		}.bind(this);
-		this.connection.onopen = this.onConnect.bind(this);
-	};
+		this.connection.onopen = function() {
+			this.onConnect();
+		}.bind(this);
+	},
 	
-	this.onConnect = function() {
+	onConnect: function() {
 		console.log("Connection established");
-	};
+	},
 	
-	this.onData = function(data) {
+	onData: function(data) {
 		console.log("Data received", data);
 		
 		if (data[0] === 8) {
 		    this.onEvent(data[1], data[2]);
 		}
-	};
+	},
 	
-	this.onDisconnect = function() {
+	onDisconnect: function() {
 		console.log("Disconnected");
-	};
+	},
 	
-	this.send = function(data) {
+	send: function(data) {
 		this.connection.send(JSON.stringify(data));
-	};
-	
-	this.connect(target);
-};
+	}
+});
